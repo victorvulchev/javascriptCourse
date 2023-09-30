320442c8a92c49306385b9296ed4e55b6f3d106e
victorvulchev <victor.vulchev@gmial.com>
2023-09-30 12:59:19 +0300

    task 7

--- homeworkArrays/task7.js
+++ homeworkArrays/task7.js
@@ -1,14 +1,23 @@
-function task(arr){
+function task(arr) {
     let longest = [];
     let i = 0;
     let command = arr[i];
-    i++
-    while(i < arr.length){
-        let tempArr = [];
-        if(command == arr[i]){
-            tempArr.push(command)
-        }
-        if(command != arr[i])
+    i++;
 
-    };
+    while (i < arr.length) {
+        let tempArr = [command];
+        while (command == arr[i]) {
+            tempArr.push(command);
+            i++;
+        }
+        if (tempArr.length > longest.length) {
+            longest = tempArr;
+        }
+        command = arr[i];
+        i++;
+    }
+
+    console.log(longest.join(" "));
 }
+
+task([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
