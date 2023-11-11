066a632bdc6fa0f194b9765fc2e9f45e944352d4
victorvulchev <victor.vulchev@gmial.com>
2023-11-11 15:14:02 +0200

    task 3

---
+++ associativeArrays/associativeArraysExercises/task3.js
@@ -0,0 +1,44 @@
+function task(input){
+    let parkingLot = [];
+
+    while(input.length != 0){
+        let current = input.shift().split(",");
+        let direction = current.shift();
+        let car = current.shift();
+        if(direction == "IN" && !parkingLot.includes(car)){
+            parkingLot.push(car);
+        }
+        else if(direction == "OUT" && parkingLot.includes(car)){
+            let index = parkingLot.indexOf(car);
+            parkingLot.splice(index, 1);
+        }
+    }
+    let sorted = parkingLot.sort((a,b) => a.localeCompare(b));
+    if(sorted.length != 0){
+        for(i of sorted){
+            console.log(i);
+        }
+    }
+    else{
+        console.log("Parking Lot is Empty")
+    }
+}
+task(['IN, CA2844AA',
+
+'IN, CA1234TA',
+
+'OUT, CA2844AA',
+
+'IN, CA9999TT',
+
+'IN, CA2866HI',
+
+'OUT, CA1234TA',
+
+'IN, CA2844AA',
+
+'OUT, CA2866HI',
+
+'IN, CA9876HH',
+
+'IN, CA2822UU'])
