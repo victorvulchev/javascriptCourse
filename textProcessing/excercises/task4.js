f703670c3777b94145ba7866a490c224e7b44901
victorvulchev <victor.vulchev@gmial.com>
2023-11-18 12:52:03 +0200

    fix

--- textProcessing/excercises/task4.js
+++ textProcessing/excercises/task4.js
@@ -1,4 +1,4 @@
-function task(word, text){
+/*function task(word, text){
     let toCheck = text.toLowerCase();
     let toFind = word.toLowerCase();
     if(toCheck.includes(toFind)){
@@ -7,7 +7,17 @@ function task(word, text){
     else{
         console.log(`${toFind} not found!`)
     }
+}*/
+function task(word, text){
+    text = text.toLowerCase().split(" ");
+    if(text.includes(word)){
+        console.log(word);
+    }
+    else{
+        console.log(`${word} not found!`)
+    }
 }
+
 task('javascript',
 
 'JavaScript is the best programming language')
